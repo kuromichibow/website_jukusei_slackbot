9d379fb28bb613769d8f301f610370a025d07ad2
RyoMitsui <ryomitsui0513@keio.jp>
2021-02-05 10:42:00 +0900

    test

---
+++ run.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+from urllib import request
+from bs4 import BeautifulSoup
+from config import config, update_recent_article
+from urllib.parse import urljoin
+import slackweb
+import ssl
+
+url = "https://www.keio.ac.jp/ja/news/2021/"
+html = request.urlopen(url)
+soup = BeautifulSoup(html, 'html.parser')
+
+infoItems = soup.find('ul', class_="infoItems")
+infoItem = infoItems.find_all('li', class_="infoItem")
+
+top_articles = []
+for item in infoItem:
+    date = item.select('span.infoDate')[0].text
+    title = item.select('span.infoText')[0].text
+    relative_link = item.select('a.infoLink')[0].get("href")
+    link = urljoin("https://www.keio.ac.jp/", relative_link)
+    top_articles.append(f'{date} | {title} | {link}')
+
+# print(top_articles)
+
+recent_article = config['web_info']['recent_article']
+
+# print(recent_article)
+
+article_list = []
+update_start = False
+
+for news in reversed(top_articles):
+    if update_start:
+        article_list.append(news)
+        continue
+    if news == recent_article:
+        update_start = True
+
+update_recent_article(news)
+
+if article_list == []:
+    article_list.append("更新記事はありません")
+
+if __name__ == "__main__":
+    slack = slackweb.Slack(url=config['slack_info']['in_webhook_url'])
+    for feed in article_list:
+        slack.notify(text=feed)
